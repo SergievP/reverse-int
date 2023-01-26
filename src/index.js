c1f2fc01b0d240e7e855ee29f7e76e78d9e2b24b
Vlad <efremov.vlad.ser@gmail.com>
2023-01-26 14:39:50 +0300

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  	let result = 0;
+  	
+  	if ( n < 0) n *= -1;
+
+  	while (n > 0) {
+    	result = result * 10 + n % 10;
+    	n = Math.floor(n / 10);
+  	}
+  	return result;
 }
